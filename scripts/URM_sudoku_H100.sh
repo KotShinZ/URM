17d28ed4f80e4d7ce01e2da856560364c3dcc354
KotShinZ <s18574s18574@gmail.com>
2026-01-13 22:39:42 +0900

    no test

--- scripts/URM_sudoku_H100.sh
+++ scripts/URM_sudoku_H100.sh
@@ -4,11 +4,11 @@ mkdir -p $checkpoint_path
 
 torchrun --nproc-per-node 1 pretrain.py \
 data_path=data/sudoku-extreme-1k-aug-1000 \
-arch=urm arch.loops=16 arch.H_cycles=2 arch.L_cycles=6 arch.num_layers=4 \
+arch=urm arch.loops=16 arch.H_cycles=2 arch.L_cycles=48 arch.num_layers=1 \
 evaluators="[]" \
 epochs=50000 \
 eval_interval=2000 \
-lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=128 \
+lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 \
 +run_name=$run_name \
 +checkpoint_path=$checkpoint_path \
 +ema=True
